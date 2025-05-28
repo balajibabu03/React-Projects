28194aff68f37105227b7552c632ed2d188c83b0
Balaji B <balajibabuece@gmail.com>
2025-05-28 23:52:04 +0530

    Tailwind css added to this project

--- Styling/src/components/Header.jsx
+++ Styling/src/components/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex flex-col items-center mt-8 mb-6" >
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
